5718a7068e0f2cc0c11cc77113145862e8796fa6
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-25 14:43:48 +0700

    [ADD] Add constant, variable, and basic arithmetic math to main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+const int firstNumber = 1;
+const int secondNumber = 2;
+
 int main() {
     printf("Welcome to Simple Calculator\n");
+
+    int firstOperand = firstNumber;
+    int secondOperand = secondNumber;
+
+    long long sum = firstOperand + secondOperand;
+    int difference = firstOperand - secondOperand;
+    long long product = firstOperand * secondOperand;
+    double quotient = firstOperand / secondOperand;
+
+    printf("-----------------------------");
+    printf("\nSum: %d + %d = %ld", firstOperand, secondOperand, sum);
+    printf("\nDifference: %d - %d = %d", firstOperand, secondOperand, difference);
+    printf("\nProduct: %d * %d = %ld", firstOperand, secondOperand, product);
+    printf("\nQuotient: %d / %d = %lf", firstOperand, secondOperand, quotient);
 }
