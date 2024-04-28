8e86f33a4bae22d2b20b87b3fd59f20fb1cb0f1f
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-28 16:49:53 +0700

    [GCD][ADD] Add implement for calculate_gcd()

--- src/main.c
+++ src/main.c
@@ -158,5 +158,11 @@ void launch_gcd() {
 }
 
 unsigned int calculate_gcd(unsigned int first_number, unsigned int second_number) {
-    return 0;
+    if (first_number == 0 || second_number == 0)
+        return (first_number + second_number);
+    
+    if (first_number > second_number) 
+        return calculate_gcd(first_number - second_number, second_number);
+    else
+        return calculate_gcd(first_number, second_number - first_number);
 }
