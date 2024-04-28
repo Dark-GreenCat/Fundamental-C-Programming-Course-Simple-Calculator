f2737000d4b4bd3cc4e8404447f8070f36ca5b6a
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-28 09:51:41 +0700

    [MODIFY] Use function prototype

--- src/main.c
+++ src/main.c
@@ -3,6 +3,28 @@
 const int default_number[] = { 1, 2 };
 const char welcome_message[] = "Welcome to Simple Calculator";
 
+int get_first_operand();
+int get_second_operand();
+char get_operator();
+void show_result(int first_operand, int second_operand, char operator);
+
+int main() {
+    printf("%s\n", welcome_message);
+
+    int first_operand = default_number[0];
+    int second_operand = default_number[1];
+    char operator;
+    int scanned_count;
+
+    printf("-----------------------------\n");
+    first_operand = get_first_operand();
+    operator = get_operator();
+    second_operand = get_second_operand();
+
+    printf("-----------------------------");
+    show_result(first_operand, second_operand, operator);
+}
+
 int get_first_operand() {
     int first_operand;
     int scanned_count;
@@ -73,21 +95,4 @@ void show_result(int first_operand, int second_operand, char operator) {
             printf("\n%-10s: %d / %d = %.15g", "Quotient", first_operand, second_operand, quotient);
         }
     }
-}
-
-int main() {
-    printf("%s\n", welcome_message);
-
-    int first_operand = default_number[0];
-    int second_operand = default_number[1];
-    char operator;
-    int scanned_count;
-
-    printf("-----------------------------\n");
-    first_operand = get_first_operand();
-    operator = get_operator();
-    second_operand = get_second_operand();
-
-    printf("-----------------------------");
-    show_result(first_operand, second_operand, operator);
 }
