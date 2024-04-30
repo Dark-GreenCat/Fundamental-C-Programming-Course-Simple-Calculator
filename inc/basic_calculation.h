09198566f39a1c0e6a481ab63e7feec004410d47
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-30 10:59:03 +0700

    [BASIC CALCULATION][MODIFY] Modify show_result() to display to console only

--- inc/basic_calculation.h
+++ inc/basic_calculation.h
@@ -6,7 +6,7 @@ double calculate(double first_operand, double second_operand, char operator);
     This function is used to calculate the result and print to console.
     The result is returned as long long type (which means double number will lose the fractional part).
 */
-long long show_result(int first_operand, int second_operand, char operator);
+void show_result(int first_operand, int second_operand, double result, char operator);
 void launch_basic_calculation();
 
 #endif
--- src/basic_calculation.c
+++ src/basic_calculation.c
@@ -12,41 +12,21 @@ double calculate(double first_operand, double second_operand, char operator) {
         case '*': return (first_operand * second_operand);
         case '/': return (first_operand / second_operand);
     }
+
+    return 0;
 }
 
-long long show_result(int first_operand, int second_operand, char operator) {
-    long long result;
-
-    if (operator == '+') {
-        long long sum = (long long) first_operand + second_operand;
-        printf("\n%-10s: %d + %d = %lld", "Sum", first_operand, second_operand, sum);
-        result = sum;
-    }
-    else if (operator == '-') {
-        int difference = first_operand - second_operand;
-        printf("\n%-10s: %d - %d = %d", "Difference", first_operand, second_operand, difference);
-        result = difference;
-    }
-    else if (operator == '*') {
-        long long product = (long long) first_operand * second_operand;
-        printf("\n%-10s: %d * %d = %lld", "Product", first_operand, second_operand, product);
-        result = product;
-    }
-    else if (operator == '/') {
-        if (second_operand == 0) {
-            printf("\nDivision by 0 is not allowed!");
-        }
-        else {
-            double quotient = ((double)first_operand) / second_operand;
-            printf("\n%-10s: %d / %d = %.15g", "Quotient", first_operand, second_operand, quotient);
-            result = (long long) quotient;
-        }
+void show_result(int first_operand, int second_operand, double result, char operator) {
+    if (operator == '/' && second_operand == 0) {
+        printf("\nDivision by 0 is not allowed!");
+        return;
     }
 
-    return result;
+    printf("\n%-10s: %d %c %d = %.15g", "Sum", first_operand, operator, second_operand, result);
 }
 
 void launch_basic_calculation() {
+    double result;
     int first_operand;
     int second_operand;
     char operator;
@@ -56,8 +36,11 @@ void launch_basic_calculation() {
         operator = (char) ui_get_input("Enter operator [+|-|*|/]", "[Error] Input should be [+|-|*|/]!", get_operator);
         second_operand = (int) ui_get_input("Enter operand", "[Error] Input should be a number!", get_operand);
         printf("-----------------------------");
-        first_operand = (int) show_result(first_operand, second_operand, operator);
+        result = calculate(first_operand, second_operand, operator);
+        show_result(first_operand, second_operand, result, operator);
         printf("\n-----------------------------");
+
+        first_operand = (int) result;
         printf("\nCurrent value: %d\n", first_operand);
     }   
 }
